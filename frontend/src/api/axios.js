f0ee0510fac9697f6d9a5dba3caf22bc3a57b994
josem-1 <161044437+josem-1@users.noreply.github.com>
2025-05-02 16:05:28 -0500

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 const BACKEND = process.env.REACT_APP_API_BASE || 'http://localhost:5000';
 
 export default axios.create({
-  baseURL: `${BACKEND}/api`,
+  baseURL: '${BACKEND}/api',
   withCredentials: true  // if you’re using cookies/auth headers
 });
