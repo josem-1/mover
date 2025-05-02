ff7fab44fb500d231c422e7b346827d91700bdbe
josem-1 <161044437+josem-1@users.noreply.github.com>
2025-05-02 15:54:39 -0500

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const BACKEND = process.env.REACT_APP_API_BASE || 'http://localhost:5000';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_BASE + '/api',       // CRA proxy will forward to localhost:5000
-  withCredentials: true, // send cookies if/when you add auth
+  baseURL: `${BACKEND}/api`,
+  withCredentials: true  // if you’re using cookies/auth headers
 });
