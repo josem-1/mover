6547faa44ccc8309e36b5a21ffafa01e0c6b4e4a
josem-1 <161044437+josem-1@users.noreply.github.com>
2025-05-06 04:40:01 -0500

    Delete frontend directory

--- frontend/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const BACKEND = process.env.REACT_APP_API_BASE || 'http://localhost:5000';
-
-export default axios.create({
-  baseURL: '${BACKEND}/api',
-  withCredentials: true  // if you’re using cookies/auth headers
-});
--- frontend/src/pages/LogoutSuccess.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function LogoutSuccess() {
-  return <h1>Successfully Logged Out</h1>;
-}
